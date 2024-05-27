c6bdfddc838f353a2354c7c0900901511bedac6a
Josecherukara <46652645+Josecherukara@users.noreply.github.com>
2024-05-27 17:42:57 +0530

    Add files via upload

---
+++ scriptRand.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    const images = document.querySelectorAll('.random-image');
+    const containerRand = document.querySelector('.containerRand');
+
+    function getRandomImage() {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        return images[randomIndex];
+    }
+
+    function displayRandomImage() {
+        const randomImage = getRandomImage();
+        containerRand.innerHTML = '';
+        containerRand.appendChild(randomImage.cloneNode());
+    }
+
+    displayRandomImage();
+
+    // Change the image every 3 seconds
+    setInterval(displayRandomImage, 3500);
+};
